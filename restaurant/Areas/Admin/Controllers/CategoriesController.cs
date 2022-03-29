5035c7ac318904ccacc2daff45b240ee2d72930f
mhmd16-ha <https://github.com/mhmd16-ha/Todo_List.git>
2022-03-29 17:18:50 +0200

    Add Categoey(CRUD) for admin area

---
+++ restaurant/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restaurant.Data;
+using restaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoriesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public async Task <IActionResult> Index()
+        {
+            var p = await db.Categories.ToListAsync();
+            return View(p);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+               await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var item = await db.Categories.FindAsync(id);
+            if (item == null) {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var item = await db.Categories.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Remove(category);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var item = await db.Categories.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+      
+
+    }
+}
--- restaurant/Data/Migrations/20220329093205_add_category_ToDb.Designer.cs
+++ restaurant/Data/Migrations/20220329093205_add_category_ToDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using restaurant.Data;
 
 namespace restaurant.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220329093205_add_category_ToDb")]
+    partial class add_category_ToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
